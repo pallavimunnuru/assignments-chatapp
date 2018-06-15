b6f7fc21dc0f73903eb47e2ffb31d88dc07bb0c7
root <root@localhost.localdomain>
2018-06-15 15:13:16 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http=require("http");
 const express=require("express");
 const socketIO=require("socket.io");
 
+const {generateMessage}=require('./utils/message');
+
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
 const app=express();
@@ -10,32 +12,22 @@ var server=http.createServer(app);
 var io=socketIO(server);
 app.use(express.static(publicPath));
 
+
+
 io.on('connection',(socket)=>{
 console.log('NEW USER CONNECTED');
 
 //socket.emit from admin text welcome to the chat app 
 
-socket.emit('newMessage',{
-    from:'Admin',
-    text:'welcome to the chat app',
-    createdAt:new Date().getTime()
-})
+socket.emit('newMessage',generateMessage('Admin','welcome to the chat app'));
 
 //socket.broadcast.emit from admin text new user joined
 
-socket.broadcast.emit('newMessage',{
-    from:'Admin',
-    text:'new user joined',
-    createdAt:new Date().getTime()
-})
+socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
 socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
-    io.emit('newMessage',{
-        from:message.from,
-        text:message.text,
-        createdAt:new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from,message.text));
 
 // socket.broadcast.emit('newMessage',{
 //     from:message.from,
---
+++ server/utils/message.test.js
@@ -0,0 +1,18 @@
+var expect=require("expect");
+var {generateMessage}=require('./message');
+
+describe('generateMessage',()=>{
+it('should generate correct message object',()=>{
+    //store res in variable
+    //assert from match
+    //assert text match
+    //assert createdAt is number
+    var from='jen';
+    var text='some message';
+    var message=generateMessage(from,text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,text})
+});
+
+});
