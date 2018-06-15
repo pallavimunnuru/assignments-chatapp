20fa024ab21728593dafccfd74099d6de619c2be
root <root@localhost.localdomain>
2018-06-15 16:50:50 +0530

    Add form from messages and show incomming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,49 @@
 console.log("disconnected from server");
 
     });
-    socket.on('newMessage',function(message){
-        console.log('newmessage',message);
 
-    })
+
+    /*
+    socket.on('newMessage',function(message){
+        console.log('newMessage',message);
+        var li=jQuery('<li></li>');
+        li.text(`${message.from}:${message.text}`);
+
+        jQuery('#messages').append(li);
+
+    });
+
+    // socket.emit('createMessage',{
+    //     from:'frank',
+    //     text:'hii'
+    // },function(data){
+    //     console.log('got it',data);
+    // });
+
+    jQuery('#message-form').on('submit',function(e){
+        e.preventDefault();
+        socket.emit('createMesage',{
+            from:'User',
+            text:jQuery('[name=message]').val()
+        },function(){
+
+        })
+
+    })*/
+    socket.on('newMessage', function (message) {
+console.log('newMessage', message);
+let li = jQuery('<li></li>');
+li.text(`${message.from}: ${message.text}`);
+
+jQuery('#messages').append(li);
+});
+    jQuery('#message-form').on('submit', function(e){
+e.preventDefault();
+
+socket.emit('createMessage', {
+from:'User',
+text: jQuery('[name=message]').val()
+}, function(){
+
+});
+});
